3ac9f03e4f9fedd17013f52260ce411af578390c
QuentBien <chauvel.quentin.95@gmail.com>
2015-11-02 16:23:09 +0100

    Mise en forme server

--- src/server/Server.java
+++ src/server/Server.java
@@ -4,23 +4,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.util.LinkedList;
 
 public class Server {
 
 	private final static int port = 8532;
+	final static int taille = 1024;
+	final static byte buffer[] = new byte[taille];
+	private static LinkedList<Vehicule> flotte;
 	
 	public static void main(String argv[]) throws Exception {
-		InetAddress serveur = InetAddress.getByName("localhost");
-		DatagramSocket socket = new DatagramSocket();
+		
+		//InetAddress serveur = InetAddress.getByName("localhost");  //à mettre dans le client pour qu'il ait l'adresse du serveur
+		DatagramSocket socket = new DatagramSocket(port);
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		String newData = "";
 		//monThread thread = new monThread();
 		//thread.start();
 		while(!newData.equals("STOP"))
 		{
+			DatagramPacket dataReceived = new DatagramPacket(buffer, buffer.length);
+			socket.receive(dataReceived);
 			newData = inFromUser.readLine();
-			DatagramPacket dataSent = new DatagramPacket(newData.getBytes(),newData.length(), serveur, port);
+			DatagramPacket dataSent = new DatagramPacket(newData.getBytes(),newData.length(),dataReceived.getAddress(),dataReceived.getPort());
 			socket.send(dataSent);
 		}
 		socket.close();
