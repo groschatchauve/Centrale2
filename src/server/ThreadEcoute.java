4c94f07df3f0c4856dc5b0d17cbd0d0099f7a62a
QuentBien <chauvel.quentin.95@gmail.com>
2015-11-02 17:12:15 +0100

    Envoie liste au client

--- src/server/ThreadEcoute.java
+++ src/server/ThreadEcoute.java
@@ -2,7 +2,7 @@ package server;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.util.LinkedList;
+import java.util.List;
 
 public class ThreadEcoute extends Thread {
 
@@ -10,10 +10,10 @@ public class ThreadEcoute extends Thread {
 	private final static byte buffer[] = new byte[taille];
 	private final static int port = 8532;
 
-	private static LinkedList<Vehicule> flotte;
+	private static List<Vehicule> flotte;
 
-	public ThreadEcoute(LinkedList<Vehicule> flotte) {
-		this.flotte = flotte;
+	public ThreadEcoute(List<Vehicule> flotte) {
+		ThreadEcoute.flotte = flotte;
 	}
 
 	@Override
@@ -22,7 +22,7 @@ public class ThreadEcoute extends Thread {
 		String listVehicules = "Liste des Véhicules disponibles : ";
 		try {
 			socket = new DatagramSocket(port);
-			DatagramPacket data = new DatagramPacket(buffer, buffer.length);
+			DatagramPacket data;
 			DatagramPacket dataReceived = new DatagramPacket(buffer, buffer.length);
 			while(true){
 				socket.receive(dataReceived);
@@ -30,7 +30,10 @@ public class ThreadEcoute extends Thread {
 					if(v.getIsDisponible())
 						listVehicules += v.getIdVehicule() + ", ";
 				}
-				//socket.send();
+				data = new DatagramPacket(listVehicules.getBytes(), listVehicules.length(), dataReceived.getAddress(), dataReceived.getPort());
+				socket.send(data);
+				socket.receive(dataReceived);
+				String commande = new String(dataReceived.getData());
 			}
 		}catch (Exception e) {e.printStackTrace();}
 	}
