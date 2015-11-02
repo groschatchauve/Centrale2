e0e445819b652141bde25f0dc1c0fbbb4bc3ee12
groschatchauve <tintin-78@hotmail.fr>
2015-11-02 17:09:59 +0100

    jeu de tests

--- src/client/TestClient.java
+++ src/client/TestClient.java
@@ -8,10 +8,30 @@ public class TestClient {
 
 	@Test
 	public void testGetVehicule() {
+		//GIVEN
 		Vehicule vehicule = new Vehicule("Audi A10", 100);
+		//WHEN
 		Reservation reservation = new Reservation("Toto", vehicule);
+		//THEN
 		assertSame(vehicule, reservation.getVehicule());
 	}
+	
+	@Test
+	public void testSetDisponibilite(){
+		//GIVEN
+		Vehicule vehicule = new Vehicule("Audi A10", 100);
+		Reservation reservation = new Reservation("Toto", vehicule);
+		//WHEN
+		Employe employe = new Employe();
+		employe.setReservation(reservation);
+		//THEN
+		long efficacite = employe.getEfficacite();
+		long tempsPreparation = employe.getReservation().getVehicule().getTempsPreparation();
+		assertSame(reservation, employe.getReservation());
+		assertSame(vehicule, employe.getReservation().getVehicule());
+		assertEquals(efficacite*tempsPreparation, efficacite * vehicule.getTempsPreparation(), 0.2);
+	}
+	
 	
 	
 
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,14 +1,26 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 public class Server {
 
-	private static LinkedList<Vehicule> flotte;
+	private static List<Vehicule> flotte = new LinkedList<>();
+	private static List<Employe> staff = new LinkedList<>();
+	private static List<Reservation> reservations = new LinkedList<>();
+	private final static int NB_MAX_EMPLOYE = 2;
 	
 	public static void main(String argv[]) throws Exception {
+		
+		//initialisation flotte
+		flotte.add(new Vehicule("Audi A10", 100));
+		flotte.add(new Vehicule("Citroen DS", 50));
+		flotte.add(new Vehicule("Ford Mustang GT", 150));
+		
+		//initialisation equipe
+		for(int i=0; i<NB_MAX_EMPLOYE; ++i)
+			staff.add(new Employe());
+
 		
 		//InetAddress serveur = InetAddress.getByName("localhost");  //à mettre dans le client pour qu'il ait l'adresse du serveur
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
